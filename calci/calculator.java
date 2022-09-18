9a782370d7388152c710233858578c7fe0468510
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-18 16:33:30 +0530

    [Nadeem] Add Addition code

---
+++ calci/calculator.java
@@ -0,0 +1,16 @@
+package com.blabz.Classroom.Practice;
+
+public class calculator {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to the Calculator Program");
+			int num1 = 10;
+			int num2 = 20;
+			int add = num1 + num2;
+			int sub = num2 - num1;
+		
+			System.out.println("Addition is: "+add);
+			System.out.println("Subtraction is: "+sub);
+			
+		}
+}
