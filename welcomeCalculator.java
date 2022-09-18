aa475b6e81cce04cce3d4af4c320b007ffff6ce6
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-18 16:30:11 +0530

    [Nadeem] Welcome message

---
+++ welcomeCalculator.java
@@ -0,0 +1,10 @@
+package com.blabz.Classroom.Practice;
+
+public class welcomeCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the Calculator code!");
+	}
+
+}
